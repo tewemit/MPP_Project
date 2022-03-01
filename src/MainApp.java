c2f026ad35a46d15646fa93809607897bceddd06
tewelle <tewelle.welemariam@cag.se>
2022-03-01 15:36:39 -0600

    Changed main class

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,11 +3,25 @@ import java.util.Scanner;
 public class MainApp {
     public static  void main(String [] args){
 
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter username");
+// write your code here
+        Scanner in = new Scanner(System.in);
+        String username;
+        String password;
+        System.out.println("========== (`_´)=======");
+        System.out.println("Welcome to MPP Library!");
+        System.out.println("User Login ");
+        System.out.print("Username: ");
+        username = in.next();
+        System.out.print("Password: ");
+        password = in.next();
+        while (in.hasNext()) {
 
-        String userName = myObj.nextLine();  // Read user input
-        System.out.println("Username is: " + userName);
+            //TODO login user
+            System.out.println("Please choose an option:");
+            System.out.println("1. Checkout book");
+            System.out.println("2. Checkin book");
+            System.out.println(" " + in.next());
+        }
 
     }
 }
